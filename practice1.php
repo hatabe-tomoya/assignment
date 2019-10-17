3717ae977c54d91a4d6b0d84fb89768615ed26eb
”hatabe_tomoya” <arkelle1559@gmail.com>
2019-10-17 03:50:25 +0000

    PHP/Laravel 03 rivised assignment

--- practice1.php
+++ practice1.php
@@ -26,10 +26,15 @@ foreach($fruits as $fruit) {
 
 // 課題４
 
-for($i = 1; $i <= 100; $i++){
+/* for文の始めの値を定義する /
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
 
   // 5で割り切れたら{}内を実行する
   if($i % 5 == 0){
     echo $i;
   }
-} ?>
+}
