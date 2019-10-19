d64f61ef87950f6260e55fa3a9e64c6539a6e361
”hatabe_tomoya” <arkelle1559@gmail.com>
2019-10-19 01:56:30 +0000

    PHP/Laravel 04 assignment

--- practice2.php
+++ practice2.php
@@ -1,6 +1,6 @@
 <?php
 
-// 課題１
+// 課題１ 引数に数値を指定して実行すると、数値を2倍にして返す関数を作成してください
 
 function double($i) {
     $result = 0;
@@ -10,7 +10,7 @@ function double($i) {
 
 echo double(10);
 
-// 課題２
+// 課題２ $a と $b を仮引数に持ち、　$a と $b　を足した結果を返す関数を作成してください
 
 function sum($a, $b) {
     $result = $a + $b;
@@ -19,7 +19,7 @@ function sum($a, $b) {
 
 echo sum(5,6);
 
-// 課題３
+// 課題３ $arr という配列の仮引数を持ち、数値が入った配列array(1, 3, 5 ,7, 9) を渡すとその要素をすべてかけた結果を返す関数を作成してください
 
 function p($arr) {
     $total = 1;
@@ -32,7 +32,7 @@ function p($arr) {
 echo p(array(1, 3, 5, 7, 9));
 
 
-// 課題４
+// 課題４ 【応用】　次のプログラムは、配列の中で一番大きい値を返す max_array という関数を実装しようとしています。途中の部分を完成させてください。
 
  function max_array($arr){
    $max_number = $arr[0];
@@ -45,7 +45,8 @@ echo p(array(1, 3, 5, 7, 9));
  }
  echo max_array(array(8, 9, 6, 5, 4, 3, 2, 1, 7));
 
-// 課題５
+// 課題５ 次のビルトイン関数の用途、使い方を調べて実際に使ってみてください。
+
 // strip_tags
 $str = "<h1>php_practice</h1>";
 echo strip_tags($str);
